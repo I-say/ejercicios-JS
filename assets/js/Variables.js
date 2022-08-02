d8d879855523ff553bfa0e6448db8f1533dca4b1
Miguel Isay <isay.morales@alumnos.udg.mx>
2022-08-02 15:09:14 -0500

    Aqui va mis apuntes de clase

---
+++ assets/js/Variables.js
@@ -0,0 +1,73 @@
+
+// Variables boleanas
+
+var aguaMty = false;
+var hambre = true;
+var lentes = true;
+var cafesito = true;
+var galletas = true;
+var luzDeDia = true;
+var sueño = true;
+var noche = false;
+var ciego = true;
+var dolorEspalda = true;
+
+
+// Variables numericas
+
+var percentageLeft = 10;
+var primeMinisterNumber = 10;
+var atomicNumberNeon = 10;
+var romanX = 10;
+var koranVowels = 10;
+var knockoutBox = 10;
+var greekDeca = 10;
+var metricSystem = 10;
+var fingersTotal = 10;
+var daysWeek = 7;
+
+// Variables string
+
+var name = "Juan";
+var lastName = "Perez";
+var municipio = "Guadalajara";
+var country = "Mexico";
+var planet = "Earth";
+var trabajo = "Programador";
+
+
+//  Bad practices
+var uno = 1;
+var second = "1s";
+
+// Why doesn't it work?
+var if_condition =  true;
+
+
+// Should equal formula for wild wings
+var Buffalowings = "wings" + "buffalo"
+
+
+// Null variable
+var vacio;
+
+
+// All variables should equal 1
+var uno, one, wan, uma, um = 1;
+
+
+// It's broken
+console.log(1);
+
+
+// Console.log para mostrar [area] de un cuadrado
+console.log(ladoa * ladob);
+
+// Should equal 4
+var four = 2 + 2;
+
+// Doesn't work for math problems
+var pi = 3.141516;
+
+
+const carritoCompras = ["Leche", "Tortillas", "Mango", "Maruchan", "Aguacate", "Sandia", "Queso", "Sabritas", "Oreo", "Piña", "Chachitos", "Pollo", "Jamon", "Harina", "Mantequilla", "Chocolate", "Wafflera Plaza Sesamo", "Nutella", "Fresas", "Servilletas", "Xbox Series X"];
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Mejores practicas
+// camelCase
+// Utilizar mayusculas en la mitad de nuestras variables
+// 
+
+
+
+alert("Hola, ya desayunaste bro?")
+
+nombre = prompt("¿Cómo te llamas?")
+
+
+console.log(nombre)
